e35edd8af74c78d609e94a902f25ec069d739de8
ShadyLane <itsashutosh.dev@gmail.com>
2024-10-31 14:53:20 +0530

    Added relevant images and redirect links

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ var typed = new Typed(".auto-type", {
     loop: true,
     typeSpeed: 150,
 })
+
+document.getElementById('project-1').addEventListener('click', ()=>{
+    window.location = 'https://aitwiki.vercel.app/index.html'
+})
+document.getElementById('project-3').addEventListener('click', ()=>{
+    window.location = 'https://calculation-project.vercel.app'
+})
+document.getElementById('social-1').addEventListener('click', ()=>{
+    window.location = 'https://github.com/justbeingashutosh'
+})
+document.getElementById('social-2').addEventListener('click', ()=>{
+    window.location = 'https://calculation-project.vercel.app'
+})
+document.getElementById('social-3').addEventListener('click', ()=>{
+    window.location = 'https://instagram.com/justbeingashutosh'
+})
+document.getElementById('social-4').addEventListener('click', ()=>{
+    window.location = 'https://calculation-project.vercel.app'
+})
